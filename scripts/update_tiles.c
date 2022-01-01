249a6e7fc8ff495ce5cc1d0782119911de25298a
Alexandru Gherasie <alexandru.gherasie@epitech.eu>
2022-01-01 15:26:49 +0100

    fixed valgrind error

--- scripts/update_tiles.c
+++ scripts/update_tiles.c
@@ -14,17 +14,8 @@ void draw_tile(game *g, sfSprite *spr, sfVector2f pos)
     sfRenderWindow_drawSprite(g->window, g->tile->spr, NULL);
 }
 
-void tile_interpretor(char **map, int y, int x)
+void spring_placement(char **map, int y, int x, int maxy)
 {
-    int maxy = 0;
-    for (maxy; map[maxy] != NULL; maxy++);
-    if (map[y][x] == 2 && map[y - 1][x] == 0)
-        map[y][x] = 3;
-    if (x > 3 && y < maxy - 1 && map[y + 1][x] == 3 && map[y][x] == 0
-    && map[y][x - 1] == 2 && map[y][x + 1] == 2)
-        map[y][x] = either_or(100, 5);
-    if (map[y][x] == 5)
-        return;
     if (y > 2 && y < maxy - 1 && map[y + 1][x] == 3
     && is_solid(map[y + 1][x]) == 0
     && is_solid(map[y][x + 1]) == 0 && is_solid(map[y - 1][x + 1]) == 0)
@@ -37,6 +28,21 @@ void tile_interpretor(char **map, int y, int x)
         map[y][x] = 104;
     if (map[y][x] == 0 && y == maxy - 1)
         map[y][x] = 6;
+}
+
+void tile_interpretor(char **map, int y, int x)
+{
+    int maxy = 0;
+    for (maxy; map[maxy] != NULL; maxy++);
+    if (map[y][x] == 2 && map[y - 1][x] == 0)
+        map[y][x] = 3;
+    if (x > 3 && y < maxy - 1 && map[y + 1][x] == 3 && map[y][x] == 0
+    && map[y][x - 1] == 2 && map[y][x + 1] == 2)
+        map[y][x] = either_or(100, 5);
+    if (map[y][x] == 5)
+        return;
+    if (map[0][x + 1] != '\0')
+        spring_placement(map, y, x, maxy);
 }
 
 void draw_game_object(game *g, sfVector2i mappos, char object)
